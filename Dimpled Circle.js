a19fe71101f01899936840bd1f4a20d5e6df9d29
James Arthur <jamesarthur7@icloud.com>
2019-02-18 16:29:37 +0000

    Create Dimpled Circle.js

---
+++ Dimpled Circle.js
@@ -0,0 +1,27 @@
+ArrayList<PVector> points = new ArrayList<PVector>();
+float a =0;
+void setup() {
+  size(400,400);
+}
+
+
+void draw() {
+  background(0);
+  translate(width/2 ,height/2);
+  stroke(175, 100, 220);
+  line(0,0,250,0);
+  noFill();
+  stroke(255);
+  beginShape();
+  for (PVector v : points) {
+    vertex(v.x, v.y);
+  }
+  endShape();
+  
+  float r = (3 + cos(a));
+  float x = 45*r*cos(a);
+  float y = 45*r*sin(a);
+  points.add(new PVector(x,y));
+  line(0,0,x,y);
+  a += 0.01;
+}
